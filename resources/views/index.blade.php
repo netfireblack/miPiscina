3f2975a1f7ad014a34302ac56936f0552068c18b
Agente-A <57039432+Agente-A@users.noreply.github.com>
2020-05-01 22:10:58 -0400

    Front End del index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,49 @@
+@extends('common')
+
+@section('page_title') Bienvenido! :D @endsection
+
+@section('jumbo_title')Piscinas:@endsection
+
+@section('jumbo_body')
+<hr>
+<div class="row">
+    <div class="col-md-4 mt-2">
+        <div class="card bg-success">
+            <img class="card-img-top" src="holder.js/100x180/" alt="">
+            <div class="card-body">
+                <h4 class="card-title">Piscina N</h4>
+                <p class="card-text">Resumen de piscina</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4 mt-2">
+        <div class="card bg-danger">
+            <img class="card-img-top" src="holder.js/100x180/" alt="">
+            <div class="card-body">
+                <h4 class="card-title">Piscina N</h4>
+                <p class="card-text">Resumen Piscina</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4 mt-2">
+        <div class="card bg-warning">
+            <img class="card-img-top" src="holder.js/100x180/" alt="">
+            <div class="card-body">
+                <h4 class="card-title">Piscina N</h4>
+                <p class="card-text">Resumen Piscina</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-4 mt-2">
+        <div class="card bg-success">
+            <img class="card-img-top" src="holder.js/100x180/" alt="">
+            <div class="card-body">
+                <h4 class="card-title">Piscina N</h4>
+                <p class="card-text">Resumen de piscina</p>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
